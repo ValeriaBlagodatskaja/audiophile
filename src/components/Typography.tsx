cb3320d739db5b76389c3e209b865ad5d5eb3419
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-20 17:45:19 +0200

    optimize Typography class structure

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -18,25 +18,20 @@ interface TypographyProps {
 
 export default function Typography({ as, children, variant }: TypographyProps) {
   const Element = as
+
+  const fontBaseClass = 'font-sans'
+
   const variantClasses = {
-    '13px':
-      'font-sans font-bold text-[13px] leading-[25px] tracking-[1px] uppercase',
-    '14px':
-      'font-sans font-normal text-[14px] leading-[19px] tracking-[10px] uppercase',
-    '15px': 'font-sans font-medium text-[15px] leading-[25px]',
-    '18px':
-      'font-sans font-bold text-[18px] leading-[24px] tracking-[1.3px] uppercase',
-    '24px':
-      'font-sans font-bold text-[24px] leading-[33px] tracking-[1.7px] uppercase',
-    '28px':
-      'font-sans font-bold text-[28px] leading-[38px] tracking-[2px] uppercase',
-    '32px':
-      'font-sans font-bold text-[32px] leading-[36px] tracking-[1.15px] uppercase',
-    '40px':
-      'font-sans font-bold text-[40px] leading-[44px] tracking-[1.5px] uppercase',
-    '56px':
-      'font-sans font-bold text-[56px] leading-[58px] tracking-[2px] uppercase',
+    '13px': 'font-bold text-[13px] leading-[25px] tracking-[1px] uppercase',
+    '14px': 'font-normal text-[14px] leading-[19px] tracking-[10px] uppercase',
+    '15px': 'font-medium text-[15px] leading-[25px]',
+    '18px': 'font-bold text-[18px] leading-[24px] tracking-[1.3px] uppercase',
+    '24px': 'font-bold text-[24px] leading-[33px] tracking-[1.7px] uppercase',
+    '28px': 'font-bold text-[28px] leading-[38px] tracking-[2px] uppercase',
+    '32px': 'font-bold text-[32px] leading-[36px] tracking-[1.15px] uppercase',
+    '40px': 'font-bold text-[40px] leading-[44px] tracking-[1.5px] uppercase',
+    '56px': 'font-bold text-[56px] leading-[58px] tracking-[2px] uppercase',
   }
-  const classNames = clsx(variantClasses[variant])
+  const classNames = clsx(fontBaseClass, variantClasses[variant])
   return <Element className={classNames}>{children}</Element>
 }
