b544ea7dc240fa199344f96a10e461940a38e3bf
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-20 16:26:25 +0200

    remove redundant div

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -11,21 +11,19 @@ interface MenuLinkProps {
 function MenuLink({ children, src }: MenuLinkProps) {
   return (
     <div className="bg-gray-light relative flex h-[165px] w-[328px] items-center justify-center rounded-lg md:w-[224px]">
-      <div className="flex flex-col items-center justify-center">
-        <img
-          className="absolute right-[76.5px] top-[-50px] md:right-[23px]"
-          src={src}
-        />
-        <div className="mt-[62px] flex flex-col gap-[17px]">
-          <Typography as="h2" variant="15px">
-            {children}
+      <img
+        className="absolute right-[76.5px] top-[-50px] md:right-[23px]"
+        src={src}
+      />
+      <div className="mt-[62px] flex flex-col gap-[17px]">
+        <Typography as="h2" variant="15px">
+          {children}
+        </Typography>
+        <LinkButton to="/" variant="tertiary">
+          <Typography as="h3" variant="13px">
+            SHOP
           </Typography>
-          <LinkButton to="/" variant="tertiary">
-            <Typography as="h3" variant="13px">
-              SHOP
-            </Typography>
-          </LinkButton>
-        </div>
+        </LinkButton>
       </div>
     </div>
   )
