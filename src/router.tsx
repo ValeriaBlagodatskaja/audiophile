f3319c61dd8118364f8e59140d35e72597314d90
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-21 12:11:43 +0200

    Set up React Router and defined routes

---
+++ src/router.tsx
@@ -0,0 +1,54 @@
+import { ReactNode } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import NavBar from './components/NavBar'
+import Earphones from './pages/Earphones'
+import Headphones from './pages/Headphones'
+import Home from './pages/Home'
+import Speakers from './pages/Speakers'
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <NavBar /> {children}
+    </>
+  )
+}
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      element: (
+        <Layout>
+          <Home />
+        </Layout>
+      ),
+      path: '/',
+    },
+    {
+      element: (
+        <Layout>
+          <Headphones />
+        </Layout>
+      ),
+      path: 'headphones',
+    },
+    {
+      element: (
+        <Layout>
+          <Speakers />
+        </Layout>
+      ),
+      path: 'speakers',
+    },
+    {
+      element: (
+        <Layout>
+          <Earphones />
+        </Layout>
+      ),
+      path: 'earphones',
+    },
+  ])
+  return <RouterProvider router={router} />
+}
