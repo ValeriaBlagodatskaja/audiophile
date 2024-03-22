128525c1baf898650588c0cd95601076ba9d4fe3
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-22 13:33:42 +0200

    add className prop, add variant

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from 'react'
 interface TypographyProps {
   as: keyof JSX.IntrinsicElements
   children: ReactNode
+  className?: string
   variant:
     | '13px'
     | '14px'
@@ -12,11 +13,17 @@ interface TypographyProps {
     | '24px'
     | '28px'
     | '32px'
+    | '36px'
     | '40px'
     | '56px'
 }
 
-export default function Typography({ as, children, variant }: TypographyProps) {
+export default function Typography({
+  as,
+  children,
+  className,
+  variant,
+}: TypographyProps) {
   const Element = as
 
   const fontBaseClass = 'font-sans'
@@ -29,9 +36,10 @@ export default function Typography({ as, children, variant }: TypographyProps) {
     '24px': 'font-bold text-[24px] leading-[33px] tracking-[1.7px] uppercase',
     '28px': 'font-bold text-[28px] leading-[38px] tracking-[2px] uppercase',
     '32px': 'font-bold text-[32px] leading-[36px] tracking-[1.15px] uppercase',
+    '36px': 'font-bold text-[36px] leading-[40px] tracking-[1.29px] uppercase',
     '40px': 'font-bold text-[40px] leading-[44px] tracking-[1.5px] uppercase',
     '56px': 'font-bold text-[56px] leading-[58px] tracking-[2px] uppercase',
   }
-  const classNames = clsx(fontBaseClass, variantClasses[variant])
+  const classNames = clsx(fontBaseClass, variantClasses[variant], className)
   return <Element className={classNames}>{children}</Element>
 }
