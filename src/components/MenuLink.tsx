bf0c82ea6a40d7b13a6f3a6a05a95698a7541ce5
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-25 14:11:29 +0200

    refactor MenuLink component to include optional srcDesktop prop for displaying images on larger screens

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -3,19 +3,30 @@ import Typography from './Typography'
 
 interface MenuLinkProps {
   children?: string
+  className?: string
   src?: string
+  srcDesktop?: string
   to: string
 }
 
-function MenuLink({ children, src, to }: MenuLinkProps) {
+function MenuLink({ children, src, srcDesktop, to }: MenuLinkProps) {
   return (
-    <div className="bg-gray-light relative flex h-[165px] w-[328px] items-center justify-center rounded-lg md:w-56">
-      <img
-        className="absolute right-[76.5px] top-[-50px] md:right-[23px]"
-        src={src}
-      />
-      <div className="mt-[62px] flex flex-col gap-[17px]">
-        <Typography as="h2" variant="15px">
+    <div className="bg-gray-light relative flex h-[165px] w-[328px] items-center justify-center rounded-lg md:w-56 xl:h-[204px] xl:w-[350px]">
+      {src && (
+        <img className="absolute top-[-50px] block  xl:hidden" src={src} />
+      )}
+      {srcDesktop && (
+        <img
+          className="absolute top-[-74px] hidden xl:block "
+          src={srcDesktop}
+        />
+      )}
+      <div className="mt-[62px] flex flex-col gap-[17px] xl:mt-[72px] xl:gap-[15px] xl:pt-[20px]">
+        <Typography
+          as="h2"
+          className="!font-bold tracking-[1.07px]"
+          variant="15px"
+        >
           {children}
         </Typography>
         <LinkButton to={to} variant="tertiary">
