57054b43bc901d75efc2d1dc2c5bd05bd18bed00
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-25 14:17:29 +0200

    update Home component to dynamically render MenuLink components with responsive image sources

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,43 @@
+import ShadowEarphones from '../assets/exported-figma/earphones-with-shadow.png'
+import ShadowEarphonesDesktop from '../assets/exported-figma/earphones-with-shadow-desktop.png'
+import ShadowHeadphones from '../assets/exported-figma/headphones-with-shadow.png'
+import ShadowHeadphonesDesktop from '../assets/exported-figma/headphones-with-shadow-desktop.png'
+import ShadowSpeakers from '../assets/exported-figma/speakers-with-shadow.png'
+import ShadowSpeakersDesktop from '../assets/exported-figma/speakers-with-shadow-desktop.png'
 import DesktopBgImage from '../assets/home/desktop/image-hero.jpg'
 import MobileBgImage from '../assets/home/mobile/image-header.jpg'
 import TabletBgImage from '../assets/home/tablet/image-header.jpg'
 import Button, { LinkButton } from '../components/Button'
 import Container from '../components/Container'
+import MenuLink from '../components/MenuLink'
 import Typography from '../components/Typography'
 
 export default function Home() {
+  const links = [
+    {
+      href: '/',
+      label: 'Home',
+    },
+    {
+      href: '/headphones',
+      label: 'Headphones',
+      src: ShadowHeadphones,
+      srcDesktop: ShadowHeadphonesDesktop,
+    },
+    {
+      href: '/speakers',
+      label: 'Speakers',
+      src: ShadowSpeakers,
+      srcDesktop: ShadowSpeakersDesktop,
+    },
+    {
+      href: '/earphones',
+      label: 'Earphones',
+      src: ShadowEarphones,
+      srcDesktop: ShadowEarphonesDesktop,
+    },
+  ]
+
   return (
     <>
       <div className="relative bg-[#191919]">
@@ -15,26 +47,24 @@ export default function Home() {
             <source media="(min-width:768px)" srcSet={TabletBgImage} />
             <img className="mt-[-115px] h-auto w-auto" src={MobileBgImage} />
           </picture>
-          <div className="xl: absolute flex flex-col items-center justify-center text-center xl:items-start xl:text-left">
+          <div className="absolute flex w-[375px] flex-col items-center justify-center text-center md:h-[346px] md:w-[398px] xl:h-[346px] xl:w-[398px] xl:items-start xl:text-left">
             <Typography as="p" className="text-white opacity-50" variant="14px">
               NEW PRODUCT
             </Typography>
             <Typography
               as="h1"
               className="mb-6 mt-4 text-white md:my-6 xl:my-6"
-              variant="36px"
+              variant="h1"
             >
-              XX99 Mark II <br />
-              HeadphoneS
+              XX99 Mark II Headphones
             </Typography>
             <Typography
               as="p"
-              className="mb-7 text-white opacity-75 md:mb-10 xl:mb-10"
+              className="mb-7 w-[328px] text-white opacity-75 md:mb-10 md:w-[349px] xl:mb-10 xl:w-[349px]"
               variant="15px"
             >
-              Experience natural, lifelike audio and <br />
-              exceptional build quality made for <br />
-              the passionate music enthusiast.
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
             </Typography>
             <LinkButton to="/" variant="primary">
               <Typography as="p" variant="13px">
@@ -45,19 +75,24 @@ export default function Home() {
         </Container>
       </div>
       <div className="relative z-10"> </div>
-      <Container>
-        <div className="my-4 flex gap-2">
-          <Button onClick={() => console.log('')} variant="primary">
-            primary
-          </Button>
-          <Button onClick={() => console.log('')} variant="secondary">
-            secondary
-          </Button>
-          <Button onClick={() => console.log('')} variant="tertiary">
-            <Typography as="p" variant="13px">
-              SHOP
-            </Typography>
-          </Button>
+      <Container className="align-center relative mb-[120px] mt-[92px] flex justify-center">
+        <div className="my-4 flex flex-col gap-[68px] md:flex-row md:gap-2.5 xl:flex-row xl:gap-[30px]">
+          {links.map((link, index) => {
+            if (link.label === 'Home') {
+              return null
+            }
+
+            return (
+              <MenuLink
+                key={index}
+                src={link.src}
+                srcDesktop={link.srcDesktop}
+                to={link.href}
+              >
+                {link.label.toUpperCase()}
+              </MenuLink>
+            )
+          })}
         </div>
       </Container>
     </>
