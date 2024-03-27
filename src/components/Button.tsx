c00c7fc9b99fc10a1f0d83978a6a878f48daea31
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-27 20:04:45 +0200

    add className prop, add quaternary variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,20 +7,23 @@ import ArrowRightIcon from '../assets/shared/desktop/icon-arrow-right.svg?react'
 interface ButtonProps {
   children: ReactNode
   onClick: () => void
-  variant: 'primary' | 'secondary' | 'tertiary'
+  variant: 'primary' | 'quaternary' | 'secondary' | 'tertiary'
 }
 
 interface LinkButtonProps {
   children: ReactNode
+  className?: string
   to: string
-  variant: 'primary' | 'secondary' | 'tertiary'
+  variant: 'primary' | 'quaternary' | 'secondary' | 'tertiary'
 }
 
 const commonClasses = {
   primary:
     'flex justify-center items-center  h-12 w-40 bg-orange-dark hover:bg-orange-light border-0 px-[30px] py-[15px] text-white',
+  quaternary:
+    'flex justify-center items-center  h-12 w-40 hover:border hover:border-black hover:bg-white hover:text-black bg-black text-white',
   secondary:
-    'flex justify-center items-center  h-12 w-40 border border-black bg-white text-black hover:bg-black hover:text-white',
+    'flex justify-center items-center  h-12 w-40 border border-black bg-transparent text-black hover:bg-black hover:text-white',
   tertiary:
     'gap-[13px] text-black opacity-50 flex items-center justify-center hover:text-orange-dark bg-transparent',
 }
@@ -34,9 +37,14 @@ export default function Button({ children, onClick, variant }: ButtonProps) {
   )
 }
 
-export function LinkButton({ children, to, variant }: LinkButtonProps) {
+export function LinkButton({
+  children,
+  className,
+  to,
+  variant,
+}: LinkButtonProps) {
   return (
-    <Link className={clsx(commonClasses[variant])} to={to}>
+    <Link className={clsx(commonClasses[variant], className)} to={to}>
       <span>{children}</span>
       {variant === 'tertiary' && <ArrowRightIcon className="flex" />}
     </Link>
