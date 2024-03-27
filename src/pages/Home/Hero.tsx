1763604e08046f0cd5156ef3540ee21fe0f6b0d9
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-27 20:00:09 +0200

    create Hero component for Home page

---
+++ src/pages/Home/Hero.tsx
@@ -0,0 +1,45 @@
+import HeroDesktopImage from '../../assets/home/desktop/image-hero.jpg'
+import HeroMobileImage from '../../assets/home/mobile/image-header.jpg'
+import HeroTabletImage from '../../assets/home/tablet/image-header.jpg'
+import { LinkButton } from '../../components/Button'
+import Container from '../../components/Container'
+import Typography from '../../components/Typography'
+
+export default function Hero() {
+  return (
+    <div className="relative bg-[#191919]">
+      <Container className="relative flex h-[510px] items-center justify-center overflow-hidden md:h-[640px]  lg:justify-between">
+        <picture className="absolute ">
+          <source media="(min-width:1100px)" srcSet={HeroDesktopImage} />
+          <source media="(min-width:768px)" srcSet={HeroTabletImage} />
+          <img className="mt-[-115px] h-auto w-auto" src={HeroMobileImage} />
+        </picture>
+        <div className="absolute flex w-[375px] flex-col items-center justify-center text-center md:h-[346px] md:w-[398px] lg:items-start lg:text-left">
+          <Typography as="p" className="text-white opacity-50" variant="14px">
+            NEW PRODUCT
+          </Typography>
+          <Typography
+            as="h1"
+            className="mb-6 mt-4 text-white md:my-6"
+            variant="h1"
+          >
+            XX99 Mark II Headphones
+          </Typography>
+          <Typography
+            as="p"
+            className="mb-7 w-[328px] text-white opacity-75 md:mb-10 md:w-[349px]"
+            variant="15px"
+          >
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </Typography>
+          <LinkButton to="/" variant="primary">
+            <Typography as="p" variant="13px">
+              SEE PRODUCT
+            </Typography>
+          </LinkButton>
+        </div>
+      </Container>
+    </div>
+  )
+}
