25163a3926881e82b45e80a136f259b5fe565665
Valeria48562 <vblagodatskaja@gmail.com>
2024-03-29 13:46:28 +0200

    add Circles svg

--- src/pages/Home/SpeakerOne.tsx
+++ src/pages/Home/SpeakerOne.tsx
@@ -1,6 +1,7 @@
 import SpeakerImageZX9Mobile from '../../assets/exported-figma/image-speaker-zx9-mobile.png'
 import SpeakerImageZX9Tablet from '../../assets/exported-figma/image-speaker-zx9-tablet.png'
 import SpeakerImageZX9Desktop from '../../assets/home/desktop/image-speaker-zx9.png'
+import Circles from '../../assets/home/desktop/pattern-circles.svg?react'
 import { LinkButton } from '../../components/Button'
 import Container from '../../components/Container'
 import Typography from '../../components/Typography'
@@ -9,12 +10,14 @@ export default function SpeakerOne() {
   return (
     <Container>
       <div className="relative flex h-[600px] flex-col items-center justify-center gap-[24px] overflow-hidden rounded-lg bg-orange-dark text-center md:h-[720px] lg:h-[560px] lg:flex-row lg:gap-[138px]">
-        <div className="absolute top-[-370px] h-[800px] w-[800px] rounded-full border-[1px] border-white border-opacity-20 md:top-[-300px] md:h-[950px] md:w-[950px] lg:left-[-82px] lg:top-[20px] lg:h-[850px] lg:w-[850px]" />
+        <div className="absolute top-[-370px] h-[800px] w-[800px] rounded-full border-[1px] border-white border-opacity-20 md:top-[-300px] md:h-[950px] md:w-[950px] lg:h-[850px] lg:w-[850px] lg:border-0" />
+
+        <Circles className="lg:absolute lg:left-[-166px] lg:top-[-60px]" />
         <div className="relative mb-[-50px] md:mb-[-120px]">
-          <div className="overfow-hidden flex h-[320px] w-[320px] items-center justify-center rounded-full border-[1px] border-white border-opacity-20 md:h-[540px] md:w-[540px] ">
+          <div className="overfow-hidden flex h-[320px] w-[320px] items-center justify-center rounded-full border-[1px] border-white border-opacity-20 md:h-[540px] md:w-[540px] lg:border-0">
             <div className="flex items-center justify-center">
-              <div className="flex h-[276px] w-[276px] items-center justify-center overflow-hidden rounded-full ring-[1px] ring-white ring-opacity-20 md:h-[450px] md:w-[450px] ">
-                <picture className="absolute lg:top-[36px]">
+              <div className="flex h-[276px] w-[276px] items-center justify-center overflow-hidden rounded-full ring-[1px] ring-white ring-opacity-20 md:h-[450px] md:w-[450px] lg:ring-0">
+                <picture className="absolute  lg:top-[20px] lg:max-w-[400px]">
                   <source
                     media="(min-width:1100px)"
                     srcSet={SpeakerImageZX9Desktop}
@@ -23,7 +26,7 @@ export default function SpeakerOne() {
                     media="(min-width:768px)"
                     srcSet={SpeakerImageZX9Tablet}
                   />
-                  <img className="h-auto w-auto" src={SpeakerImageZX9Mobile} />
+                  <img src={SpeakerImageZX9Mobile} />
                 </picture>
               </div>
             </div>
