d16217cbea74cbba131bd0bcab1448457dabd2ec
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-01 13:54:29 +0300

    create Product component

---
+++ src/components/Product.tsx
@@ -0,0 +1,74 @@
+import clsx from 'clsx'
+
+import { LinkButton } from '../components/Button'
+import Typography from '../components/Typography'
+
+export interface ProductProps {
+  alignment?: string
+  description: string
+  newProduct: boolean
+  srcSet: {
+    lg: string
+    md: string
+    sm: string
+  }
+  title: string
+  to: string
+}
+
+export default function Product({
+  alignment,
+  description,
+  newProduct,
+  srcSet,
+  title,
+  to,
+}: ProductProps) {
+  console.log(alignment)
+  return (
+    <>
+      <div
+        className={clsx(
+          'flex flex-col items-center justify-center lg:gap-[125px]',
+          alignment === 'right' ? 'lg:flex-row' : 'lg:flex-row-reverse'
+        )}
+      >
+        <picture className="mb-8  mt-16 md:mb-[52px] md:mt-[120px] lg:mt-40">
+          <source media="(min-width:1100px)" srcSet={srcSet.lg} />
+          <source media="(min-width:768px)" srcSet={srcSet.md} />
+          <img className="" srcSet={srcSet.sm} />
+        </picture>
+        <div className="flex flex-col items-center lg:mt-40 lg:flex lg:h-[560px] lg:flex-col lg:justify-center">
+          {newProduct && (
+            <Typography
+              as="span"
+              className="mb-6 text-orange-dark"
+              variant="14px"
+            >
+              new product
+            </Typography>
+          )}
+          <Typography
+            as="h2"
+            className="w-[200px] text-center lg:text-left"
+            variant="h2"
+          >
+            {title}
+          </Typography>
+          <Typography
+            as="p"
+            className="my-6 w-[327px] text-center opacity-50 md:mb-6 md:mt-8 md:w-[572px] lg:mb-10 lg:w-[445px] lg:text-left"
+            variant="15px"
+          >
+            {description}
+          </Typography>
+          <LinkButton className="mb-14" to={to} variant="primary">
+            <Typography as="p" variant="13px">
+              SEE PRODUCT
+            </Typography>
+          </LinkButton>
+        </div>
+      </div>
+    </>
+  )
+}
