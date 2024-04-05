4c6204b71cc20df4e48f6e5483cdfa517579b7ad
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-05 20:39:16 +0300

    reorganize Home component structure and layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,27 @@
 import Footer from '../../components/Footer'
-import Earphones from '../Home/Earphones'
-import Hero from '../Home/Hero'
-import ProductLinks from '../Home/ProductLinks'
-import SpeakerOne from '../Home/SpeakerOne'
-import SpeakerTwo from '../Home/SpeakerTwo'
-import Store from '../Home/Store'
+import Earphones from './components/Earphones'
+import Hero from './components/Hero'
+import ProductLinks from './components/ProductLinks'
+import SpeakerOne from './components/SpeakerOne'
+import SpeakerTwo from './components/SpeakerTwo'
+import Store from './components/Store'
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <div className="relative z-10"> </div>
-      <ProductLinks />
-      <SpeakerOne />
-      <SpeakerTwo />
-      <Earphones />
-      <Store />
-      <Footer />
+      <div className="flex flex-col gap-[92px] md:gap-[148px] lg:gap-[200px]">
+        <Hero />
+        <ProductLinks />
+      </div>
+      <div className="flex flex-col gap-6 md:gap-8 lg:gap-12">
+        <SpeakerOne />
+        <SpeakerTwo />
+        <Earphones />
+      </div>
+      <div className="flex flex-col gap-[120px] md:gap-24 lg:gap-[200px] ">
+        <Store />
+        <Footer />
+      </div>
     </>
   )
 }
