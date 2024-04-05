5f05aa7b61ef715d220bf99b9e8ca653fe965258
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-05 19:01:34 +0300

    replace the LinkButton with the ShopLink component and updating the Typography component props

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -1,39 +1,39 @@
-import { LinkButton } from './Button'
+import ShopLink from '../components/ShopLink'
 import Typography from './Typography'
 
 interface MenuLinkProps {
   children?: string
   className?: string
-  src?: string
-  srcDesktop?: string
+  onClick?: () => void
+  srcSet: {
+    lg: string
+    md: string
+    sm: string
+  }
   to: string
 }
 
-function MenuLink({ children, src, srcDesktop, to }: MenuLinkProps) {
+function MenuLink({ children, onClick, srcSet, to }: MenuLinkProps) {
   return (
     <div className="relative flex h-[165px] w-[327px] items-center justify-center rounded-lg bg-gray-light md:w-56 lg:h-[204px] lg:w-[350px]">
-      {src && (
-        <img className="absolute top-[-50px] block  lg:hidden" src={src} />
-      )}
-      {srcDesktop && (
-        <img
-          className="absolute top-[-74px] hidden lg:block "
-          src={srcDesktop}
-        />
-      )}
+      <picture className="absolute top-[-50px] lg:top-[-74px]">
+        <source media="(min-width:1100px)" srcSet={srcSet.lg} />
+        <source media="(min-width:768px)" srcSet={srcSet.md} />
+        <img srcSet={srcSet.sm} />
+      </picture>
       <div className="mt-[62px] flex flex-col gap-[17px] lg:mt-[72px] lg:gap-[15px] lg:pt-[20px]">
         <Typography
           as="h2"
           className="!font-bold tracking-[1.07px]"
-          variant="15px"
+          color="15px"
         >
           {children}
         </Typography>
-        <LinkButton to={to} variant="tertiary">
-          <Typography as="h3" variant="13px">
+        <ShopLink onClick={onClick} to={to}>
+          <Typography as="h3" color="13px">
             SHOP
           </Typography>
-        </LinkButton>
+        </ShopLink>
       </div>
     </div>
   )
