86c019e802655dd6c5e710dc6e6a7b0c4ec8b506
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-05 15:32:44 +0300

    use data and remap to match props

--- src/pages/Earphones.tsx
+++ src/pages/Earphones.tsx
@@ -1,3 +1,27 @@
+import data from '../../data.json'
+import { ProductProps } from '../components/Product'
+import ProductListPage from '../components/ProductListPage'
+
 export default function Earphones() {
-  return <h1>Earphones</h1>
+  const earphones = data.filter(({ category }) => category === 'earphones')
+
+  const remapDataToMatchProps = () => {
+    return earphones.map((product) => {
+      console.log('product', product)
+      const productObject: ProductProps = {
+        description: product.description,
+        newProduct: product.new,
+        srcSet: {
+          lg: product.categoryImage.desktop,
+          md: product.categoryImage.tablet,
+          sm: product.categoryImage.mobile,
+        },
+        title: product.name,
+        to: '',
+      }
+      return productObject
+    })
+  }
+  const remappedData = remapDataToMatchProps()
+  return <ProductListPage products={remappedData} title="Earphones" />
 }
--- src/pages/Speakers.tsx
+++ src/pages/Speakers.tsx
@@ -1,41 +1,30 @@
-import SpeakerZX7Desktop from '../assets/exported-figma/speaker-zx7-preview-desktop.png'
-import SpeakerZX7Mobile from '../assets/exported-figma/speaker-zx7-preview-mobile.png'
-import SpeakerZX7Tablet from '../assets/exported-figma/speaker-zx7-preview-tablet.png'
-import SpeakerZX9Desktop from '../assets/exported-figma/speaker-zx9-preview-desktop.png'
-import SpeakerZX9Mobile from '../assets/exported-figma/speaker-zx9-preview-mobile.png'
-import SpeakerZX9Tablet from '../assets/exported-figma/speaker-zx9-preview-tablet.png'
+import data from '../../data.json'
+import { ProductProps } from '../components/Product'
 import ProductListPage from '../components/ProductListPage'
 
 export default function Speakers() {
-  return (
-    <ProductListPage
-      products={[
-        {
-          description:
-            'Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.',
-          newProduct: true,
-          srcSet: {
-            lg: SpeakerZX9Desktop,
-            md: SpeakerZX9Tablet,
-            sm: SpeakerZX9Mobile,
-          },
-          title: 'ZX9 SPEAKER',
-          to: 'toote leht',
+  const speakers = data.filter(({ category }) => category === 'speakers')
+
+  const remapDataToMatchProps = () => {
+    const sortedSpeakers = [...speakers].sort((a, b) =>
+      a.new === b.new ? 0 : a.new ? -1 : 1
+    )
+
+    return sortedSpeakers.map((product) => {
+      const productObject: ProductProps = {
+        description: product.description,
+        newProduct: product.new,
+        srcSet: {
+          lg: product.categoryImage.desktop,
+          md: product.categoryImage.tablet,
+          sm: product.categoryImage.mobile,
         },
-        {
-          description:
-            'Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.',
-          newProduct: false,
-          srcSet: {
-            lg: SpeakerZX7Desktop,
-            md: SpeakerZX7Tablet,
-            sm: SpeakerZX7Mobile,
-          },
-          title: 'ZX7 SPEAKER',
-          to: 'toote leht',
-        },
-      ]}
-      title="Speakers"
-    />
-  )
+        title: product.name,
+        to: '',
+      }
+      return productObject
+    })
+  }
+  const remappedData = remapDataToMatchProps()
+  return <ProductListPage products={remappedData} title="Speakers" />
 }
