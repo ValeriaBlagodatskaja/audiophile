aa64007b4be0893a427be0e40378957a30aa5688
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-15 13:43:08 +0300

    Updated GoBackLink component to use react-router-dom's useNavigate hook for navigation

--- src/components/GoBackLink.tsx
+++ src/components/GoBackLink.tsx
@@ -1,24 +1,31 @@
 import { ReactNode } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { LinkButton } from '../components/Button'
 import Typography from '../components/Typography'
+import Container from './Container'
 
 interface GoBackLinkProps {
   children: ReactNode
-  onClick?: () => void
-  to?: string
 }
 
-export default function GoBackLink({ children, onClick, to }: GoBackLinkProps) {
+export default function GoBackLink({ children }: GoBackLinkProps) {
+  const navigate = useNavigate()
+
+  const handleGoBack = () => {
+    navigate(-1)
+  }
   return (
-    <Typography
-      as="p"
-      className="flex items-start pt-4 md:pt-8 lg:pt-20"
-      variant="15px"
-    >
-      <LinkButton color="gray" onClick={onClick} to={to}>
-        {children}{' '}
-      </LinkButton>
-    </Typography>
+    <Container>
+      <Typography
+        as="p"
+        className="flex items-start pb-6 pt-4 md:pt-8 lg:pb-14 lg:pt-20"
+        variant="15px"
+      >
+        <LinkButton color="gray" onClick={handleGoBack}>
+          {children}{' '}
+        </LinkButton>
+      </Typography>
+    </Container>
   )
 }
