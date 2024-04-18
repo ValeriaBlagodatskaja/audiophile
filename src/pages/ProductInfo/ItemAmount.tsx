2dc21c64613367039dce8d47811e7d7923a24c84
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-18 15:13:28 +0300

    refactor ItemAmount component

--- src/pages/ProductInfo/ItemAmount.tsx
+++ src/pages/ProductInfo/ItemAmount.tsx
@@ -1,28 +1,49 @@
+import clsx from 'clsx'
+
 import Button from '../../components/Button'
 
 interface ItemAmountProps {
   amount: number
-  setAmount: React.Dispatch<React.SetStateAction<number>>
+  className?: string
+
+  updateQuantity: (newQuantity: number) => void
 }
 
-export default function ItemAmount({ amount, setAmount }: ItemAmountProps) {
+export default function ItemAmount({
+  amount,
+  className,
+  updateQuantity,
+}: ItemAmountProps) {
   const increaseAmount = () => {
-    setAmount(amount + 1)
+    updateQuantity(amount + 1)
   }
 
   const decreaseAmount = () => {
     if (amount > 1) {
-      setAmount(amount - 1)
+      updateQuantity(amount - 1)
     }
   }
 
   return (
-    <div className="flex h-12 w-[120px] place-content-around items-center	 bg-gray-light">
-      <Button color="gray" onClick={decreaseAmount}>
+    <div
+      className={clsx(
+        'flex h-12 w-[120px] place-content-around items-center bg-gray-light',
+        className
+      )}
+    >
+      <Button
+        className="h-12 w-[60px] text-[20px]"
+        color="gray"
+        onClick={decreaseAmount}
+      >
         -
       </Button>
-      <span className="text-[13px] font-bold">{amount}</span>
-      <Button color="gray" onClick={increaseAmount}>
+      <span className="text-[15px] font-bold">{amount}</span>
+      <Button
+        className="h-12 w-[60px] text-[20px]"
+        color="gray"
+        onClick={increaseAmount}
+      >
         +
       </Button>
     </div>
