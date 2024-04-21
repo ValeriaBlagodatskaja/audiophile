13b580af351dafdbcac2461edced1f5294a32a55
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-21 22:31:58 +0300

    create Checkout component

---
+++ src/components/Checkout/Checkout.tsx
@@ -0,0 +1,13 @@
+import GoBackLink from '../GoBackLink'
+import Form from './Form'
+import Summary from './Summary'
+
+export default function Checkout() {
+  return (
+    <>
+      <GoBackLink>Go Back</GoBackLink>
+      <Form />
+      <Summary />
+    </>
+  )
+}
