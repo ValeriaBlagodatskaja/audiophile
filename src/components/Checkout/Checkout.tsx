ab06d668baf1836f8f0aa1fb564c335df7540697
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-24 12:45:59 +0300

    Update Checkout component to use react-hook-form, reorganize component structure

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,16 +1,39 @@
+import { useForm } from 'react-hook-form'
+
 import Container from '../Container'
 import GoBackLink from '../GoBackLink'
-import Form from './Form/Form'
+import Typography from '../Typography'
+import BillingDetails from './Form/BillingDetails'
+import PaymentDetails from './Form/PaymentDetails'
+import ShippingInfo from './Form/ShippingInfo'
 import Summary from './Summary'
 
 export default function Checkout() {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm()
+
+  const onSubmit = (data: any) => {
+    console.log('Form submitted with data:', data)
+  }
   return (
     <>
       <GoBackLink>Go Back</GoBackLink>
       <Container>
         <div className="flex flex-col gap-8 lg:flex-row">
-          <Form />
-          <Summary />
+          <form className="flex flex-1 flex-col justify-evenly gap-8 rounded-lg bg-white px-[28px] py-[30px] md:gap-[54px] lg:px-12 lg:py-[54px]">
+            <div className="flex flex-col gap-8 md:gap-10">
+              <Typography as="h2" variant="h2-2">
+                Checkout
+              </Typography>
+              <BillingDetails errors={errors} register={register} />
+            </div>
+            <ShippingInfo errors={errors} register={register} />
+            <PaymentDetails errors={errors} register={register} />
+          </form>
+          <Summary onContinue={handleSubmit(onSubmit)} />
         </div>
       </Container>
     </>
