b3a445cd29d9c42be38ede7dbc99b2dc790e8921
Valeria48562 <vblagodatskaja@gmail.com>
2024-04-30 15:53:50 +0300

    updated Modal component to allow for toggling the behavior of closing the modal on click outside

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,6 +7,7 @@ import useClickOutside from '../hooks/useClickOutside'
 interface ModalProps {
   children: ReactNode
   className?: string
+  closeOnClickOutside?: boolean
   open: boolean
   setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
@@ -14,14 +15,17 @@ interface ModalProps {
 export default function Modal({
   children,
   className,
+  closeOnClickOutside = true,
   open,
   setOpen,
 }: ModalProps) {
   const modalContentRef = useRef(null)
-  useClickOutside(modalContentRef, open, () => setOpen(false))
+  useClickOutside(modalContentRef, closeOnClickOutside ? open : false, () =>
+    setOpen(false)
+  )
 
   if (!open) {
-    return <div className="hidden" />
+    return null
   }
 
   return ReactDom.createPortal(
@@ -29,7 +33,7 @@ export default function Modal({
       <div className="z-1000 fixed inset-0 bg-black bg-opacity-40" />
       <div
         className={clsx(
-          'z-1000 md-custom:px-8 md-custom:w-[377px] fixed left-1/2 right-0 top-1/2 mx-auto w-[327px] -translate-x-1/2 transform rounded-[8px] bg-white px-3 py-8',
+          'z-1000 md-custom:px-8 md-custom:w-[377px] absolute left-1/2 right-0 top-1/2 mx-auto w-[327px] -translate-x-1/2 transform rounded-[8px] bg-white px-3 py-8',
           className
         )}
         ref={modalContentRef}
