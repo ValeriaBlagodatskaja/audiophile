eae46c87a1f61c14c69868ceedb79759a2195b2a
Valeria48562 <vblagodatskaja@gmail.com>
2024-05-09 12:49:48 +0300

    install vite-tsconfig-paths plugin, add base URL and paths to tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default {
-  plugins: [react(), svgr()],
+  plugins: [tsconfigPaths(), react(), svgr()],
 }
