1c60eefcefbb892f248c2743a01b91003fca05e2
Valeria48562 <vblagodatskaja@gmail.com>
2024-05-17 15:28:58 +0300

    improve animation in Hero component

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -4,80 +4,63 @@ import HeroTabletImage from '@/assets/home/tablet/image-header.jpg'
 import { LinkButton } from '@/components/Button'
 import Container from '@/components/Container'
 import Typography from '@/components/Typography'
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 export default function Hero() {
+  const fromTopMotion = {
+    hidden: { opacity: 0, transition: { duration: 0.5 }, y: -20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  }
+
   return (
     <div className="relative bg-[#191919]">
       <Container className="relative flex h-[510px] items-center justify-center overflow-hidden md:h-[640px]  lg:justify-between">
-        <picture className="absolute ">
+        <motion.picture
+          className="absolute "
+          initial="hidden"
+          variants={fromTopMotion}
+          whileInView="visible"
+        >
           <source media="(min-width:1100px)" srcSet={HeroDesktopImage} />
           <source media="(min-width:768px)" srcSet={HeroTabletImage} />
           <img className="mt-[-115px] h-auto w-auto" src={HeroMobileImage} />
-        </picture>
-        <div className="absolute flex w-[375px] flex-col items-center justify-center text-center md:h-[346px] md:w-[398px] lg:items-start lg:text-left">
-          <AnimatePresence>
-            <motion.div
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              initial={{ opacity: 0, y: -20 }}
-              key="new-product"
-              transition={{ delay: 0.5, duration: 0.5 }}
-            >
-              <Typography
-                as="p"
-                className="text-white opacity-50"
-                variant="14px"
-              >
-                New product
-              </Typography>
-            </motion.div>
-            <motion.div
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              initial={{ opacity: 0, y: -20 }}
-              key="xx99-headphones"
-              transition={{ delay: 1, duration: 0.5 }}
-            >
-              <Typography
-                as="h1"
-                className="mb-6 mt-4 text-white md:my-6"
-                variant="36px-56px"
-              >
-                XX99 Mark II headphones
-              </Typography>
-            </motion.div>
-            <motion.div
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              initial={{ opacity: 0, y: -20 }}
-              key="product-features"
-              transition={{ delay: 1.5, duration: 0.5 }}
-            >
-              <Typography
-                as="p"
-                className="mb-7 w-full max-w-[327px] text-white opacity-75 md:mb-10 md:max-w-[349px]"
-                variant="15px"
-              >
-                Experience natural, lifelike audio and exceptional build quality
-                made for the passionate music enthusiast.
-              </Typography>
-            </motion.div>
-            <motion.div
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              initial={{ opacity: 0, y: -20 }}
-              key="see-product"
-              transition={{ delay: 2, duration: 0.5 }}
-            >
-              <LinkButton color="orange" to="/xx99-mark-two-headphones">
-                <Typography as="p" variant="13px">
-                  See product
-                </Typography>
-              </LinkButton>
-            </motion.div>
-          </AnimatePresence>
-        </div>
+        </motion.picture>
+        <motion.div
+          className="absolute flex w-[375px] flex-col items-center justify-center text-center md:h-[346px] md:w-[398px] lg:items-start lg:text-left"
+          initial="hidden"
+          variants={fromTopMotion}
+          whileInView="visible"
+        >
+          <Typography as="p" className="text-white opacity-50" variant="14px">
+            New product
+          </Typography>
+
+          <Typography
+            as="h1"
+            className="mb-6 mt-4 text-white md:my-6"
+            variant="36px-56px"
+          >
+            XX99 Mark II headphones
+          </Typography>
+
+          <Typography
+            as="p"
+            className="mb-7 w-full max-w-[327px] text-white opacity-75 md:mb-10 md:max-w-[349px]"
+            variant="15px"
+          >
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </Typography>
+
+          <LinkButton color="orange" to="/xx99-mark-two-headphones">
+            <Typography as="p" variant="13px">
+              See product
+            </Typography>
+          </LinkButton>
+        </motion.div>
       </Container>
     </div>
   )
