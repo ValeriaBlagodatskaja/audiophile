fd0eb7fa3e7f1045392fb6c381df8456cbfdf5aa
Valeria48562 <vblagodatskaja@gmail.com>
2024-05-17 23:09:25 +0300

    add animation to CheckoutPage

--- src/pages/Checkout/CheckoutPage.tsx
+++ src/pages/Checkout/CheckoutPage.tsx
@@ -5,9 +5,9 @@ import BillingDetails from '@/pages/Checkout/components/BillingDetails'
 import PaymentDetails from '@/pages/Checkout/components/PaymentDetails'
 import ShippingInfo from '@/pages/Checkout/components/ShippingInfo'
 import Summary from '@/pages/Checkout/components/Summary'
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-
 export interface CheckoutFormData {
   address: string
   city: string
@@ -33,12 +33,36 @@ export default function Checkout() {
   const onSubmit = () => {
     setIsFormCompleted(Object.keys(errors).length === 0)
   }
+
+  const fromTopMotion = {
+    hidden: { opacity: 0, y: -20 },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.5 },
+      y: 0,
+    },
+  }
+  const fromTopMotionDelayed = {
+    hidden: { opacity: 0, y: -20 },
+    visible: {
+      opacity: 1,
+      transition: { delay: 0.5, duration: 0.5 },
+      y: 0,
+    },
+  }
+
   return (
     <>
       <GoBackLink />
       <Container>
         <div className="flex flex-col gap-8 lg:flex-row">
-          <form className="flex flex-1 flex-col justify-evenly gap-8 rounded-lg bg-white px-7 py-[30px] md:gap-[54px] lg:px-12 lg:py-[54px]">
+          <motion.form
+            className="flex flex-1 flex-col justify-evenly gap-8 rounded-lg bg-white px-7 py-[30px] md:gap-[54px] lg:px-12 lg:py-[54px]"
+            initial="hidden"
+            variants={fromTopMotion}
+            viewport={{ once: true }}
+            whileInView="visible"
+          >
             <div className="flex flex-col gap-8 md:gap-10">
               <Typography as="h2" variant="28px-32px">
                 Checkout
@@ -47,11 +71,18 @@ export default function Checkout() {
             </div>
             <ShippingInfo errors={errors} register={register} />
             <PaymentDetails errors={errors} register={register} />
-          </form>
-          <Summary
-            isFormCompleted={isFormCompleted}
-            onContinue={handleSubmit(onSubmit)}
-          />
+          </motion.form>
+          <motion.div
+            initial="hidden"
+            variants={fromTopMotionDelayed}
+            viewport={{ amount: 0.2, once: true }}
+            whileInView="visible"
+          >
+            <Summary
+              isFormCompleted={isFormCompleted}
+              onContinue={handleSubmit(onSubmit)}
+            />
+          </motion.div>
         </div>
       </Container>
     </>
