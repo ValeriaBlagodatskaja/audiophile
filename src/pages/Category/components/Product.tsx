846d59ca49f08fe7c697759dd23dbd666aa47c85
Valeria48562 <vblagodatskaja@gmail.com>
2024-05-17 17:59:44 +0300

    improve animation in Product component

--- src/pages/Category/components/Product.tsx
+++ src/pages/Category/components/Product.tsx
@@ -24,29 +24,39 @@ export default function Product({
   title,
   to,
 }: ProductProps) {
-  const delayChildren = 0.5
-  const staggerChildren = 0.5
-
   const container = {
     hidden: { opacity: 1, scale: 0 },
     visible: {
       opacity: 1,
       scale: 1,
       transition: {
-        delayChildren,
+        delayChildren: 0.5,
         ease: 'easeInOut',
-        staggerChildren,
+        staggerChildren: 0.5,
       },
     },
   }
 
-  const riseUpVariant = {
+  const slideInLeftVariant = {
     container,
     item: {
-      hidden: { opacity: 0, y: 20 },
+      hidden: { opacity: 0, x: -100 },
       visible: {
         opacity: 1,
-        y: 0,
+        transition: { duration: 0.5, ease: 'easeInOut' },
+        x: 0,
+      },
+    },
+  }
+
+  const slideInRightVariant = {
+    container,
+    item: {
+      hidden: { opacity: 0, x: 100 },
+      visible: {
+        opacity: 1,
+        transition: { duration: 0.5, ease: 'easeInOut' },
+        x: 0,
       },
     },
   }
@@ -58,19 +68,19 @@ export default function Product({
         alignment === 'left' ? 'lg:flex-row' : 'lg:flex-row-reverse'
       )}
       initial="hidden"
-      variants={riseUpVariant.container}
+      variants={container}
       viewport={{ amount: 0.2, once: true }}
       whileInView="visible"
     >
-      <motion.div variants={riseUpVariant.item}>
+      <motion.div variants={slideInLeftVariant.item}>
         <picture className="max-w-[380px] md:max-w-[780px]">
           <source media="(min-width:1100px)" srcSet={srcSet.lg} />
           <source media="(min-width:768px)" srcSet={srcSet.md} />
-          <img srcSet={srcSet.sm} />
+          <img className="rounded-lg" srcSet={srcSet.sm} />
         </picture>
       </motion.div>
       <div className="flex flex-col items-center lg:flex lg:h-[560px] lg:items-start lg:justify-center lg:text-left">
-        <motion.div variants={riseUpVariant.item}>
+        <motion.div variants={slideInRightVariant.item}>
           {newProduct && (
             <Typography
               as="span"
@@ -80,8 +90,7 @@ export default function Product({
               New product
             </Typography>
           )}
-        </motion.div>
-        <motion.div variants={riseUpVariant.item}>
+
           <Typography
             as="h1"
             className="text-center lg:text-left"
@@ -89,8 +98,7 @@ export default function Product({
           >
             {title}
           </Typography>
-        </motion.div>
-        <motion.div variants={riseUpVariant.item}>
+
           <Typography
             as="p"
             className="my-6 w-full text-center opacity-50 md:mb-6 md:mt-8 md:max-w-[572px] lg:mb-10 lg:max-w-none  lg:text-left"
@@ -98,8 +106,7 @@ export default function Product({
           >
             {description}
           </Typography>
-        </motion.div>
-        <motion.div variants={riseUpVariant.item}>
+
           <LinkButton color="orange" to={to}>
             <Typography as="p" variant="13px">
               See product
