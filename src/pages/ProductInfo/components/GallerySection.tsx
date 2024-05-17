470fc6b1d21741f82a4c61bbd789a021a606bb61
Valeria48562 <vblagodatskaja@gmail.com>
2024-05-17 12:53:54 +0300

    add animation to GallerySection

--- src/pages/ProductInfo/components/GallerySection.tsx
+++ src/pages/ProductInfo/components/GallerySection.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 interface GallerySectionProps {
   galleryImages: {
     lg: string
@@ -11,24 +13,49 @@ export default function GallerySection({ galleryImages }: GallerySectionProps) {
 
   const thirdImage = galleryImages[2]
 
+  const containerVariants = {
+    hidden: { opacity: 0, rotate: -10 },
+    visible: {
+      opacity: 1,
+      rotate: 0,
+      transition: {
+        delayChildren: 0.2,
+        duration: 0.5,
+        ease: 'easeInOut',
+        staggerChildren: 0.2,
+      },
+    },
+  }
+
+  const itemVariants = {
+    hidden: { opacity: 0, rotate: -10 },
+    visible: { opacity: 1, rotate: 0 },
+  }
+
   return (
-    <div className="flex flex-col gap-5 md:flex-row lg:gap-[30px]">
+    <motion.div
+      className="flex flex-col gap-5 md:flex-row lg:gap-[30px]"
+      initial="hidden"
+      variants={containerVariants}
+      viewport={{ amount: 0.2, once: true }}
+      whileInView="visible"
+    >
       <div className="flex flex-col gap-5 md:justify-center lg:gap-[30px]">
         {displayedImages.map((galleryImage, index) => (
-          <picture key={index}>
+          <motion.picture key={index} variants={itemVariants}>
             <source media="(min-width:1100px)" srcSet={galleryImage.lg} />
             <source media="(min-width:768px)" srcSet={galleryImage.md} />
             <img className="rounded-lg " srcSet={galleryImage.sm} />
-          </picture>
+          </motion.picture>
         ))}
       </div>
       {thirdImage && (
-        <picture>
+        <motion.picture variants={itemVariants}>
           <source media="(min-width:1100px)" srcSet={thirdImage.lg} />
           <source media="(min-width:768px)" srcSet={thirdImage.md} />
           <img className="rounded-lg" srcSet={thirdImage.sm} />
-        </picture>
+        </motion.picture>
       )}
-    </div>
+    </motion.div>
   )
 }
