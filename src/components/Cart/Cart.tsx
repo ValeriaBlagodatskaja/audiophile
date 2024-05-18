88e4b3360e821ed3c23549d036c4f38af3851061
ValeriaBlagodatskaja <vblagodatskaja@gmail.com>
2024-05-18 14:08:29 +0300

    add no items in cart state

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -41,48 +41,59 @@ const Cart = ({ open, setOpen }: CartProps) => {
           </TextButton>
         )}
       </div>
-      <ul className="flex flex-col gap-6 py-8">
-        {cartItems.map((item) => (
-          <li
-            className="flex flex-row items-center justify-between"
-            key={item.id}
-          >
-            <div className="flex flex-row items-center gap-1 md-custom:gap-4">
-              {item.srcSet && item.srcSet.sm && (
-                <img className="h-16 w-16 rounded-lg" srcSet={item.srcSet.sm} />
-              )}
-              <div>
-                <Typography
-                  as="p"
-                  className="w-[110px] overflow-hidden text-ellipsis whitespace-nowrap font-bold uppercase"
-                  variant="15px"
-                >
-                  {item.title}
-                </Typography>
-                <Typography
-                  as="p"
-                  className="text-[14px] opacity-50"
-                  variant="15px"
-                >
-                  {numbro(item.price).formatCurrency('$ 0,0')}
-                </Typography>
+      {cartItems && cartItems.length > 0 && (
+        <ul className="flex flex-col gap-6 py-8">
+          {cartItems.map((item) => (
+            <li
+              className="flex flex-row items-center justify-between"
+              key={item.id}
+            >
+              <div className="flex flex-row items-center gap-1 md-custom:gap-4">
+                {item.srcSet && item.srcSet.sm && (
+                  <img
+                    className="h-16 w-16 rounded-lg"
+                    srcSet={item.srcSet.sm}
+                  />
+                )}
+                <div>
+                  <Typography
+                    as="p"
+                    className="w-[110px] overflow-hidden text-ellipsis whitespace-nowrap font-bold uppercase"
+                    variant="15px"
+                  >
+                    {item.title}
+                  </Typography>
+                  <Typography
+                    as="p"
+                    className="text-[14px] opacity-50"
+                    variant="15px"
+                  >
+                    {numbro(item.price).formatCurrency('$ 0,0')}
+                  </Typography>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-row gap-1">
-              <ItemAmount
-                amount={item.quantity}
-                className="h-8 w-[96px]"
-                updateQuantity={(newQuantity) =>
-                  updateCartItemQuantity(item.id, newQuantity)
-                }
-              />
-              <button onClick={() => removeFromCart(item.id)}>
-                <img className="w-[15px]" src={DeleteIcon4} />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+              <div className="flex flex-row gap-1">
+                <ItemAmount
+                  amount={item.quantity}
+                  className="h-8 w-[96px]"
+                  updateQuantity={(newQuantity) =>
+                    updateCartItemQuantity(item.id, newQuantity)
+                  }
+                />
+                <button onClick={() => removeFromCart(item.id)}>
+                  <img className="w-[15px]" src={DeleteIcon4} />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {totalItems === 0 && (
+        <Typography as="p" className="py-8 opacity-50" variant="15px">
+          No items in your cart.
+        </Typography>
+      )}
       <div className="flex flex-col gap-6">
         <div className="flex flex-row justify-between">
           <Typography as="h3" className="opacity-50" variant="15px">
