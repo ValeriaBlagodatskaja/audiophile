e5200cc73c7b407fdc8e6a4f05954da4cd050c6c
ValeriaBlagodatskaja <vblagodatskaja@gmail.com>
2024-05-18 12:21:40 +0300

    fix bug where clicking on the X button closes and then immediately reopens the navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { links } from '@/constants/links'
 import { useCart } from '@/hooks/useCart'
+import clsx from 'clsx'
 import { AnimatePresence, motion } from 'framer-motion'
 import { Spin as Hamburger } from 'hamburger-react'
 import { useEffect, useRef, useState } from 'react'
@@ -14,11 +15,7 @@ import MenuLink from './MenuLink'
 import Modal from './Modal'
 import Typography from './Typography'
 
-interface NavBarProps {
-  closeOnClickOutside?: boolean
-}
-
-function NavBar({ closeOnClickOutside = true }: NavBarProps) {
+function NavBar() {
   const [isOpen, setOpen] = useState(false)
 
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -75,9 +72,7 @@ function NavBar({ closeOnClickOutside = true }: NavBarProps) {
   }
 
   const modalContentRef = useRef(null)
-  useClickOutside(modalContentRef, closeOnClickOutside ? isOpen : false, () =>
-    setOpen(false)
-  )
+  useClickOutside(modalContentRef, isOpen, () => setOpen(false))
 
   const getCartProductAmount = () => {
     let amount = 0
@@ -91,7 +86,13 @@ function NavBar({ closeOnClickOutside = true }: NavBarProps) {
     <div className="sticky top-0 z-20 lg:bg-[#191919]">
       <Container className="relative z-30 bg-[#191919]">
         <div className="flex h-[90px] items-center justify-between border-b-[1px] border-white border-opacity-20 ">
-          <div className="lg:hidden">
+          <div
+            className={clsx(
+              'lg:hidden',
+              // useClickOutside hook to closes the menu when clicking outside
+              isOpen && 'pointer-events-none'
+            )}
+          >
             <Hamburger
               color="white"
               direction="right"
@@ -164,11 +165,11 @@ function NavBar({ closeOnClickOutside = true }: NavBarProps) {
           <>
             <motion.div
               animate="visible"
-              className="fixed top-0 z-10 h-full w-full  bg-black bg-opacity-40 "
+              className="fixed top-0 z-10 h-full w-full bg-black bg-opacity-40"
               exit="hidden"
               initial="hidden"
               variants={backdropVariants}
-            ></motion.div>
+            />
             <motion.div
               animate="visible"
               className="absolute z-10 flex w-full flex-col items-center gap-[68px]  rounded-b-lg bg-white pb-[35px] pt-[84px] md:flex-row md:justify-center md:gap-2.5 md:pb-[67px] md:pt-[108px] "
@@ -177,21 +178,20 @@ function NavBar({ closeOnClickOutside = true }: NavBarProps) {
               ref={modalContentRef}
               variants={modalVariants}
             >
-              {links.map((link, index) => {
-                if (link.label === 'Home' || !link.src) {
-                  return null
-                }
-                return (
-                  <MenuLink
-                    key={index}
-                    onClick={() => setOpen(false)}
-                    src={link.src}
-                    to={link.href}
-                  >
-                    {link.label.toUpperCase()}
-                  </MenuLink>
-                )
-              })}
+              {links
+                .filter((link) => Boolean(link.src))
+                .map((link, index) => {
+                  return (
+                    <MenuLink
+                      key={index}
+                      onClick={() => setOpen(false)}
+                      src={link.src!}
+                      to={link.href}
+                    >
+                      {link.label.toUpperCase()}
+                    </MenuLink>
+                  )
+                })}
             </motion.div>
           </>
         )}
